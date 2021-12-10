f98f363f17a9f620b5f66576ac2acc681672c7e3
Anastasia <92372917+AnastasiaChizhova1@users.noreply.github.com>
2021-12-10 20:44:15 +0300

    Add files via upload

---
+++ bbo.py
@@ -0,0 +1,14 @@
+nam_1 = int(input('Enter your first number: ')) 
+nam_2 = int(input('Enter your second number: ')) 
+# Сложение
+print('{} + {} = '.formnam_1t(nam_1, nam_2)) 
+print(nam_1 + nam_2)
+# Вычетание 
+print('{} - {} = '.formnam_1t(nam_1, nam_2)) 
+print(nam_1 - nam_2)
+# Умножение 
+print('{} * {} = '.formnam_1t(nam_1, nam_2)) 
+print(nam_1 * nam_2)
+# Деление 
+print('{} / {} = '.formnam_1t(nam_1, nam_2)) 
+print(nam_1 / nam_2)
